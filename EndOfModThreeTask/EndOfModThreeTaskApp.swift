24b8b9873862e1e980766b78e8b7e5d9409fa36f
Grace Gong <ggong@lcs.on.ca>
2023-04-20 09:39:10 -0400

    Initial Commit

---
+++ EndOfModThreeTask/EndOfModThreeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  EndOfModThreeTaskApp.swift
+//  EndOfModThreeTask
+//
+//  Created by Grace Gong on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct EndOfModThreeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
